a77462884f7386d832a5f750b35a328341a83d8e
vjking007 <Freelancing@97177>
2025-07-25 15:01:37 +0530

    Product Swagger semicolon issue

--- productservice/src/main/java/com/example/product_service/config/SwaggerConfig.java
+++ productservice/src/main/java/com/example/product_service/config/SwaggerConfig.java
@@ -1,5 +1,5 @@
 package com.example.product_service.config;
-;
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
