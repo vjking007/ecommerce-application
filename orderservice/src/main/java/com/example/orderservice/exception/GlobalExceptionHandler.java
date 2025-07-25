64465aeac30d53e12430b38698ba2a14af07301f
vjking007 <Freelancing@97177>
2025-07-25 19:48:00 +0530

    updated order global exception

--- orderservice/src/main/java/com/example/orderservice/exception/GlobalExceptionHandler.java
+++ orderservice/src/main/java/com/example/orderservice/exception/GlobalExceptionHandler.java
@@ -34,15 +34,5 @@ public class GlobalExceptionHandler {
         );
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
-
-    @ExceptionHandler(InsufficientStockException.class)
-    public ResponseEntity<ErrorResponse> handleStockException(InsufficientStockException ex) {
-        ErrorResponse error = new ErrorResponse(
-                HttpStatus.BAD_REQUEST.value(),
-                ex.getMessage(),
-                LocalDateTime.now()
-        );
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-    }
 }
 
