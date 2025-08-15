eacc6741b550105ab2c6b256507d4ddbcd52e498
vjking007 <Freelancing@97177>
2025-08-15 18:27:47 +0530

    Cart and product updated code

--- ecommerce-angular-app/src/app/cart/cart.module.ts
+++ ecommerce-angular-app/src/app/cart/cart.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { CartRoutingModule } from './cart-routing.module';
 import { CartComponent } from './cart/cart.component';
 
